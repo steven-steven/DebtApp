e1c67e88b9b4221e4b113505bcbb70c61975d5b8
Polina Govorkova <polinka.lmb@gmail.com>
2017-02-18 16:28:24 -0500

    Created password asking page/proper names

---
+++ app/src/main/java/com/example/hp_pc/debtapp/Summary_entrance_page.java
@@ -0,0 +1,13 @@
+package com.example.hp_pc.debtapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Summary_entrance_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary_entrance_page);
+    }
+}
