5c129e074a44e401984b4dc1529a7d2989510b53
Steven <steven.infinity29@gmail.com>
2017-02-18 17:04:59 -0500

    Edit the Gradle App file & The mainActivity Java File

--- app/src/main/java/com/example/hp_pc/debtapp/Summary_entrance_page.java
+++ app/src/main/java/com/example/hp_pc/debtapp/Summary_entrance_page.java
@@ -3,11 +3,24 @@ package com.example.hp_pc.debtapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class Summary_entrance_page extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary_entrance_page);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(2, 1),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
     }
 }
