fa114ce4fb48908fbf398956c05f7a67ecad306e
Steven <steven.infinity29@gmail.com>
2017-02-18 18:41:30 -0500

    Edit the Gradle App file & The mainActivity Java File

---
+++ app/src/main/java/com/example/hp_pc/debtapp/Entry.java
@@ -0,0 +1,21 @@
+package com.example.hp_pc.debtapp;
+
+import static android.R.attr.x;
+
+/**
+ * Created by HP-PC on 2/18/2017.
+ */
+
+public class Entry {
+
+    protected int dataX;
+    protected int dayaY;
+
+    public int getDataX() {
+        return dataX;
+    }
+
+    public int getDayaY() {
+        return dayaY;
+    }
+}
